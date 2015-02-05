893a99e60543ece11f4f3920c52a7a81b4525674
Peter Saxton <peterhsaxton@gmail.com>
2015-02-05 16:17:17 +0000

    stashin catalogue number

--- lib/piece/record.rb
+++ lib/piece/record.rb
@@ -1,5 +1,10 @@
 class Piece
   class Record < Sequel::Model(:pieces)
-
+    plugin :serialization
+    # TODO catalogue_number class throw error when providing bad value
+    serialize_attributes [
+      lambda{|v| v.match(/UD(\d{4})/)[1].to_i},
+      lambda{|v| "UD%04d" % v}
+    ], :catalogue_number
   end
 end
--- test/piece/record_test.rb
+++ test/piece/record_test.rb
@@ -2,6 +2,16 @@ require_relative '../test_config'
 
 class Piece
   class RecordTest < MiniTest::Test
+    def values
+
+    end
+    def test_can_save_a_catalogue_number
+      assert_silent do
+        skip
+        r = Record.new :catalogue_number => 'UD001'
+        r.save
+      end
+    end
     def test_requires_catalogue_number
       assert_raises Sequel::NotNullConstraintViolation do
         Record.create
