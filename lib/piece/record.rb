2621831269b48857d004d87ccbdb99dd629d41ef
Peter Saxton <peterhsaxton@gmail.com>
2015-02-08 23:29:24 +0000

    catalogue number as identifier

--- lib/piece/record.rb
+++ lib/piece/record.rb
@@ -5,6 +5,7 @@ require_relative './uploaders/notation_preview_uploader'
 class Piece
   class Record < Sequel::Model(:pieces)
     unrestrict_primary_key
+    primary_key = :catalogue_number
 
     one_to_many :item_records, :class => :'Item::Record', :key => :piece_id
 
@@ -17,5 +18,10 @@ class Piece
     mount_uploader :cover_image, CoverImageUploader
     mount_uploader :audio_preview, AudioPreviewUploader
     mount_uploader :notation_preview, NotationPreviewUploader
+
+    def id
+      catalogue_number
+    end
+    
   end
 end
