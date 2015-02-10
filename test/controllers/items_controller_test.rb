e18e4e09e9b5f1ad2b4b488406bdce7a6d0c7e8a
Peter Saxton <peterhsaxton@gmail.com>
2015-02-10 23:07:35 +0000

    controller tests for item

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -10,13 +10,12 @@ class ItemsControllerTest < MyRecordTest
   def test_new_page_is_available
     record = create :piece_record
     assert_ok get "/new?piece_id=#{record.id}"
+    assert_includes last_response.body, "UD#{record.id}"
   end
 
   def test_can_create_new_item
-    skip
     piece_record = create :piece_record
     post '/', :item => attributes_for(:item_record).merge(:piece => piece_record.id)
-    ap last_response.status
-    assert_equal '/items', last_response.location
+    assert_equal "/pieces/UD#{piece_record.id}", last_response.location
   end
 end
