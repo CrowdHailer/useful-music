3d42d435f8f8c358fa638ee7eaaf0e15f9ae12ef
Peter Saxton <peterhsaxton@gmail.com>
2015-02-10 18:07:09 +0000

    subsequent price access

--- test/item/entity_test.rb
+++ test/item/entity_test.rb
@@ -38,5 +38,20 @@ class Item
       item.initial_price = full_price
       assert_equal full_price, record.initial_price
     end
+
+    def test_can_access_subsequent_price
+      record.subsequent_price = full_price
+      assert_equal full_price, item.subsequent_price
+    end
+
+    def test_uses_initial_price_if_subsequent_price_undefined
+      record.initial_price = full_price
+      assert_equal full_price, item.subsequent_price
+    end
+
+    def test_can_set_subsequent_price
+      item.subsequent_price = full_price
+      assert_equal full_price, record.subsequent_price
+    end
   end
 end
