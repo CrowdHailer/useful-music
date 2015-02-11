8895ae50f5c4ce901e29845f78095900207695ba
Peter Saxton <peterhsaxton@gmail.com>
2015-02-11 08:55:33 +0000

    test filename for item asset

--- test/item/record_test.rb
+++ test/item/record_test.rb
@@ -22,11 +22,22 @@ class Item
       end
     end
 
+    def test_can_save_asset
+      record = create :item_record
+      assert record.asset.path
+    end
+
     def test_requires_asset
       err = assert_raises Sequel::NotNullConstraintViolation do
         create :item_record, :asset => nil
       end
       assert_match(/asset/, err.message)
+    end
+
+    def test_renames_asset_file
+      record = create :item_record, :name => 'piano solo'
+      piece_id = record.piece_record.id
+      assert_match /pieces\/UD#{piece_id}\/items\/UD#{piece_id}_piano_solo\.mp3$/, record.asset.path
     end
   end
 end
