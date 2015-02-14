331c5d006ac01577cd9b1af03bccc1bc58b5076b
Peter Saxton <peterhsaxton@gmail.com>
2015-02-14 11:11:12 +0000

    clean piece entity

--- lib/piece/record.rb
+++ lib/piece/record.rb
@@ -5,10 +5,9 @@ require_relative './uploaders/notation_preview_uploader'
 class Piece
   class Record < Sequel::Model(:pieces)
     unrestrict_primary_key
-    # primary_key = :catalogue_number
 
     one_to_many :item_records, :class => :'Item::Record', :key => :piece_id
-    
+
     plugin :association_dependencies, :item_records => :delete
 
     plugin :serialization
@@ -20,10 +19,6 @@ class Piece
     mount_uploader :cover_image, CoverImageUploader
     mount_uploader :audio_preview, AudioPreviewUploader
     mount_uploader :notation_preview, NotationPreviewUploader
-
-    # def id
-    #   catalogue_number
-    # end
 
   end
 end
