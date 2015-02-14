8b21f12de3a8690d56e83dff4ee8e7ec3c3bd1f7
Peter Saxton <peterhsaxton@gmail.com>
2015-02-14 23:09:58 +0000

    create customer form

---
+++ lib/customer/create/form.rb
@@ -0,0 +1,14 @@
+class Customer
+  class Create
+    class Form
+      include Virtus.model
+
+      attribute :first_name, String
+      attribute :last_name, String
+      attribute :email, String
+      attribute :password, String
+      attribute :country, String
+
+    end
+  end
+end
