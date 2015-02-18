748c5c1a5b0c1eccba6bcc224bbe8bab4ab102ac
Peter Saxton <peterhsaxton@gmail.com>
2015-02-18 13:55:31 +0000

    clean basket controller

--- app/controllers/shopping_baskets.rb
+++ app/controllers/shopping_baskets.rb
@@ -11,27 +11,4 @@ class ShoppingBasketsController < UsefulMusic::App
     render :show
   end
 
-  delete '/*/purchase/*' do |basket_id, purchase_id|
-    # "#{basket_id} + #{purchase_id}"
-    Purchase::Record[purchase_id].destroy
-    redirect "/shopping_baskets/#{basket_id}"
-  end
-
-  post '/add_purchases' do
-    if session['useful_music.basket_id']
-      basket = ShoppingBasket::Record[session['useful_music.basket_id']]
-    else
-      basket = ShoppingBasket::Record.create
-      session['useful_music.basket_id'] = basket.id
-    end
-    add_form.each do |purchase_data|
-      purchase_record = Purchase::Record.new purchase_data
-      basket.add_purchase_record purchase_record
-    end
-    redirect "/shopping_baskets/#{basket.id}"
-  end
-
-  def add_form
-     request.POST['purchases']
-  end
 end
