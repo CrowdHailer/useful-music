cb9ccc4de7c8e00fd1898c306266d284addda247
Peter Saxton <peterhsaxton@gmail.com>
2015-02-18 14:16:54 +0000

    purchase based shopping basket edit

--- app/controllers/purchases.rb
+++ app/controllers/purchases.rb
@@ -30,6 +30,7 @@ class PurchasesController < UsefulMusic::App
   include Scorched::Rest
 
   def create
+    # TODO error tests
     form = Purchase::CreateMany::Form.new request.POST
     form.purchases.each do |create_form|
       begin
@@ -43,5 +44,22 @@ class PurchasesController < UsefulMusic::App
         purchase_record.save
       end
     end
+  end
+
+  def update(id)
+    # TODO error tests
+    purchase = Purchase.new(Purchase::Record[id])
+    purchase.quantity = request.POST['purchase']['quantity']
+    purchase.record.save
+    flash[:success] = 'Shopping basket updated'
+    redirect request.referer
+  end
+
+  def destroy(id)
+    # TODO error tests
+    purchase = Purchase.new(Purchase::Record[id])
+    purchase.record.destroy
+    flash[:success] = 'Item removed from basket'
+    redirect request.referer
   end
 end
