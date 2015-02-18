b40ad4deea0d92272da04ec63a0ee06fe73f1a35
Peter Saxton <peterhsaxton@gmail.com>
2015-02-18 09:03:13 +0000

    create new piece with instrument options

--- lib/piece/create.rb
+++ lib/piece/create.rb
@@ -9,6 +9,22 @@ class Piece
       attribute :sub_heading, String
       attribute :description, String
       attribute :category, String
+      attribute :beginner, Boolean
+      attribute :intermediate, Boolean
+      attribute :advanced, Boolean
+      attribute :professional, Boolean
+      attribute :piano, Boolean
+      attribute :recorder, Boolean
+      attribute :flute, Boolean
+      attribute :oboe, Boolean
+      attribute :clarineo, Boolean
+      attribute :clarinet, Boolean
+      attribute :basson, Boolean
+      attribute :saxophone, Boolean
+      attribute :trumpet, Boolean
+      attribute :violin, Boolean
+      attribute :viola, Boolean
+      attribute :percussion, Boolean
       attribute :notation_preview, Hash
       attribute :audio_preview, Hash
       attribute :cover_image, Hash
