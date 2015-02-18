4a91d11385f7d6b73633c722b960b9d64897ce44
Peter Saxton <peterhsaxton@gmail.com>
2015-02-18 10:38:29 +0000

    shopping basket create record

--- db/migrations/003_create_shopping_baskets.rb
+++ db/migrations/003_create_shopping_baskets.rb
@@ -2,6 +2,8 @@ Sequel.migration do
   up do
     create_table(:shopping_baskets) do
       primary_key :id
+      DateTime :created_at
+      DateTime :updated_at
     end
   end
 
--- lib/shopping_basket/record.rb
+++ lib/shopping_basket/record.rb
@@ -1,5 +1,7 @@
 class ShoppingBasket
   class Record < Sequel::Model(:shopping_baskets)
+    plugin :timestamps, :update_on_create=>true
+
     one_to_many :purchase_records, :class => :'Purchase::Record', :key => :shopping_basket_id
   end
 end
