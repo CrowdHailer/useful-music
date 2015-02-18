407ed6f78a5b62236e58b51ab2df5afa0e3df5f6
Peter Saxton <peterhsaxton@gmail.com>
2015-02-18 09:25:55 +0000

    information routes

--- app/controllers/about.rb
+++ app/controllers/about.rb
@@ -4,4 +4,36 @@ class AboutController < UsefulMusic::App
   get '/' do
     render :index
   end
+
+  get '/composers' do
+    render :composers
+  end
+
+  get '/search_and_purchase' do
+    render :search_and_purchase
+  end
+
+  get '/licencing_and_copyright' do
+    render :licencing_and_copyright
+  end
+
+  get '/refunds' do
+    render :refunds
+  end
+
+  get '/privacy' do
+    render :privacy
+  end
+
+  get '/terms_and_conditions' do
+    render :terms_and_conditions
+  end
+
+  get '/contact' do
+    render :contact
+  end
+
+  get '/sitemap' do
+    render :sitemap
+  end
 end
