08f358a29d7b96a3e49acf8cc3d9dd8c1e98e75d
Peter Saxton <peterhsaxton@gmail.com>
2015-02-20 09:33:25 +0000

    validate user formating

--- lib/customer/create/validator.rb
+++ lib/customer/create/validator.rb
@@ -3,11 +3,23 @@ class Customer
     class Validator
       include Veto.validator
 
-      validates :first_name, :presence => true
-      validates :last_name, :presence => true
-      validates :email, :presence => true
-      validates :password, :presence => true
-      validates :country, :presence => true
+      validates :first_name,
+        :presence => true,
+        :min_length => 2,
+        :max_length => 26
+      validates :last_name,
+        :presence => true,
+        :min_length => 2,
+        :max_length => 26
+      validates :email,
+        :presence => true,
+        :format => /^[^@]+@[^@]+$/
+      validates :password,
+        :presence => true,
+        :min_length => 2,
+        :max_length => 55
+      validates :country,
+        :presence => true
 
     end
   end
