ef8c6dda3601013792c6c6c1498acd1d6500a8f1
Peter Saxton <peterhsaxton@gmail.com>
2015-02-26 16:37:04 +0000

    record tests for order

--- lib/order/record.rb
+++ lib/order/record.rb
@@ -7,5 +7,12 @@ class Order
 
     many_to_one :shopping_basket_record, :class => :'ShoppingBasket::Record', :key => :shopping_basket_id
     many_to_one :customer_record, :class => :'Customer::Record', :key => :customer_id
+
+    plugin :serialization
+
+    serialize_attributes [
+      lambda{ |money| money.fractional },
+      lambda{ |fractional| Money.new(fractional) }
+    ], :basket_amount, :tax_amount, :discount_amount
   end
 end
--- test/factories/order_record.rb
+++ test/factories/order_record.rb
@@ -2,7 +2,10 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :order_record, :class => Order::Record do
-    state :something
+    state :pending
+    basket_amount Money.new(2)
+    tax_amount Money.new(2)
+    discount_amount Money.new(2)
     shopping_basket_record
     customer_record
   end
