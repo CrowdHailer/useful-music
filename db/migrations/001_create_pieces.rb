a1ee11fbdb52052bbea766b526790cec53958f84
Peter Saxton <peterhsaxton@gmail.com>
2015-02-27 15:21:14 +0000

    move constraints table to first migration

--- db/migrations/001_create_pieces.rb
+++ db/migrations/001_create_pieces.rb
@@ -1,5 +1,9 @@
 Sequel.migration do
   up do
+    extension(:constraint_validations)
+
+    DB.create_constraint_validations_table
+
     create_table(:pieces) do
       primary_key :id, :type => :integer, :auto_increment => false, :unique => true
       String :title, :null => false
@@ -33,6 +37,10 @@ Sequel.migration do
   end
 
   down do
+    extension(:constraint_validations)
+
     drop_table(:pieces)
+
+    DB.drop_constraint_validations_table
   end
 end
