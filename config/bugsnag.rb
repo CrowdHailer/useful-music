abdf6aa33fd774c242bc3876f61bd92ec2d1a847
Peter Saxton <peterhsaxton@gmail.com>
2015-03-02 20:51:23 +0000

    add customer error handling

--- config/bugsnag.rb
+++ config/bugsnag.rb
@@ -1,3 +1,8 @@
 Bugsnag.configure do |config|
   config.api_key = ENV.fetch('BUGSNAG_API_KEY')
 end
+Bugsnag.before_notify_callbacks << lambda {|notif|
+  notif.add_tab(:user_info, {
+    name: current_customer.name
+  })
+}
