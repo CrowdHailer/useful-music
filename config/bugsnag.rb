6807366e825fc2835b7cccd4e96ccd0dd8d5955b
Peter Saxton <peterhsaxton@gmail.com>
2015-03-02 21:37:29 +0000

    config path

--- config/bugsnag.rb
+++ config/bugsnag.rb
@@ -1,5 +1,6 @@
 Bugsnag.configure do |config|
   config.api_key = ENV.fetch('BUGSNAG_API_KEY')
+  config.project_root = File.expand_path('../..', __FILE__)
 end
 # Bugsnag.before_notify_callbacks << lambda {|notif|
 #   notif.user = {
