521b0fbc39e82fe21bed6eb3b7558c10c380dc63
Peter Saxton <peterhsaxton@gmail.com>
2015-03-04 17:54:00 +0000

    discounts create page

--- lib/discounts.rb
+++ lib/discounts.rb
@@ -2,8 +2,12 @@ require_relative './discount/record'
 
 class Discounts < Errol::Repository
   require_relative './discounts/query'
-  
+
   record_class ::Discount::Record
   entity_class ::Discount
   query_class Query
+
+  def implant(record)
+    Discount.new(record)
+  end
 end
