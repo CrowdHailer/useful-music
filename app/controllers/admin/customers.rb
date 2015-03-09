341a99cbad959cb78c9401705e429c778a8b07ee
Peter Saxton <peterhsaxton@gmail.com>
2015-03-09 16:38:43 +0000

    action to make admin

--- app/controllers/admin/customers.rb
+++ app/controllers/admin/customers.rb
@@ -2,6 +2,9 @@ module UsefulMusic
   module Admin
     class CustomersController < App
       include Scorched::Rest
+      post('/:id/admin') { |id| send :make_admin, id }
+      delete('/:id/change_password') { |id| send :remove_admin, id }
+
       render_defaults[:dir] += '/admin/customers'
       render_defaults[:layout] = File.expand_path('app/views/admin/application', APP_ROOT).to_sym
 
@@ -9,6 +12,20 @@ module UsefulMusic
         @customers = Customers.new request.GET
         render :index
       end
+
+      def make_admin(id)
+        customer = Customers.fetch(id, &method(:customer_not_found))
+        customer.set :admin => true
+        Customers.save customer
+        flash['success'] = "#{customer.name} is now an admin"
+        redirect '/admin/customers'
+      end
+
+      def customer_not_found(id)
+        flash['error'] = 'Customer not found'
+        redirect '/admin/customers'
+      end
+
     end
   end
 end
--- test/admin/customer_controller_test.rb
+++ test/admin/customer_controller_test.rb
@@ -19,6 +19,20 @@ module UsefulMusic
         get '/'
         assert_includes last_response.body, 'Issac'
       end
+
+      def test_can_make_admin
+        customer_record = create :customer_record
+        post "/#{customer_record.id}/admin"
+        assert customer_record.reload.admin
+        assert flash['success']
+        assert last_response.redirect?
+      end
+
+      def test_redirects_if_customer_not_found
+        post "/1/admin"
+        assert 'Customer not found', flash['error']
+        assert last_response.redirect?
+      end
     end
   end
 end
