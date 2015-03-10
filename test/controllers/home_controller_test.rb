706ac05522a11cb5b6120d8e8453e9997f30f152
Peter Saxton <peterhsaxton@gmail.com>
2015-03-10 20:21:08 +0000

    test home redirect

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -11,4 +11,99 @@ class HomeControllerTest < MyRecordTest
     assert_ok get '/'
   end
 
+  def test_redirects_to_piano_search
+    get '/piano'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_woodwind_search
+    get '/woodwind'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_recorder_search
+    get '/recorder'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_flute_search
+    get '/flute'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_oboe_search
+    get '/oboe'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_clarineo_search
+    get '/clarineo'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_clarinet_search
+    get '/clarinet'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_bassoon_search
+    get '/bassoon'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_saxophone_search
+    get '/saxophone'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_trumpet_search
+    get '/trumpet'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_violin_search
+    get '/violin'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_viola_search
+    get '/viola'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_percussion_search
+    get '/percussion'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_solo_search
+    get '/solo'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_solo_with_accompaniment_search
+    get '/solo_with_accompaniment'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_duet_search
+    get '/duet'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_trio_search
+    get '/trio'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_quartet_search
+    get '/quartet'
+    assert last_response.redirect?
+  end
+
+  def test_redirects_to_larger_ensembles_search
+    get '/larger_ensembles'
+    assert last_response.redirect?
+  end
+
 end
