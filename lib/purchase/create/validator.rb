31e544effb970b82b1dedf26a8dd2b830e2be378
Peter Saxton <peterhsaxton@gmail.com>
2015-03-11 09:19:26 +0000

    validator fix for empty basket

--- lib/purchase/create/validator.rb
+++ lib/purchase/create/validator.rb
@@ -5,8 +5,8 @@ class Purchase
       include Veto.validator
 
       validates :item, :presence => true
-      validates :shopping_basket, :presence => true
-      validates :quantity, :greater_than => 0 
+      validates :shopping_basket, :not_null => true
+      validates :quantity, :greater_than => 0
     end
   end
 end
