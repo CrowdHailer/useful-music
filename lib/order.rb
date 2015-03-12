6646d0ea13be7473481ab721f4149f23902a310d
Peter Saxton <peterhsaxton@gmail.com>
2015-03-12 15:36:57 +0000

    order has customer

--- lib/order.rb
+++ lib/order.rb
@@ -97,67 +97,73 @@ end
 class Order < Errol::Entity
   require_relative './order/record'
 
+  entry_accessor  :state
+
   def initialize(*args)
     super
+    record.state ||= 'pending'
   end
 
-  def mark_pending
-    self.record.state = 'pending'
-  end
-
-  def transaction
-    @transaction ||= Transaction.new(record)
-  end
-
-  entry_accessor  :basket_amount,
-                  :tax_amount,
-                  :discount_amount,
-                  :updated_at,
-                  :created_at
-
-  def calculate_prices
-    self.basket_amount = shopping_basket.price
-    self.tax_amount = basket_amount * customer.vat_rate
-    self.discount_amount = Money.new(discount.value) if discount
-    self.discount_amount = Money.new(0) if !discount
-  end
-
-  def to_pay
-    basket_amount + tax_amount - discount_amount
-  end
-
-  delegate :setup, :fetch_details, :checkout, :to => :transaction
-
-
-  def shopping_basket
-    ShoppingBasket.new record.shopping_basket_record if record.shopping_basket_record
-  end
-
-  def shopping_basket=(shopping_basket)
-    record.shopping_basket_record = shopping_basket.record
-  end
-
-  def discount=(discount)
-    # TODO FIX
-    return
-    if discount.nil?
-      record.discount_record = nil
-    else
-      record.discount_record = discount.record
-    end
-  end
-
-  def discount
-    # TODO FIX
-    return
-    Discount.new record.discount_record if record.discount_record
-  end
-
+  # def mark_pending
+  #   self.record.state = 'pending'
+  # end
+  #
+  # def transaction
+  #   @transaction ||= Transaction.new(record)
+  # end
+  #
+  #                 :tax_amount,
+  #                 :discount_amount,
+  #                 :updated_at,
+  #                 :created_at
+  #
+  # def calculate_prices
+  #   self.basket_amount = shopping_basket.price
+  #   self.tax_amount = basket_amount * customer.vat_rate
+  #   self.discount_amount = Money.new(discount.value) if discount
+  #   self.discount_amount = Money.new(0) if !discount
+  # end
+  #
+  # def to_pay
+  #   basket_amount + tax_amount - discount_amount
+  # end
+  #
+  # delegate :setup, :fetch_details, :checkout, :to => :transaction
+  #
+  #
+  # def shopping_basket
+  #   ShoppingBasket.new record.shopping_basket_record if record.shopping_basket_record
+  # end
+  #
+  # def shopping_basket=(shopping_basket)
+  #   record.shopping_basket_record = shopping_basket.record
+  # end
+  #
+  # def discount=(discount)
+  #   # TODO FIX
+  #   return
+  #   if discount.nil?
+  #     record.discount_record = nil
+  #   else
+  #     record.discount_record = discount.record
+  #   end
+  # end
+  #
+  # def discount
+  #   # TODO FIX
+  #   return
+  #   Discount.new record.discount_record if record.discount_record
+  # end
+  #
   def customer
     Customer.new record.customer_record if record.customer_record
   end
 
   def customer=(customer)
-    record.customer_record = customer.record
+    if customer.nil?
+      record.customer_record = customer
+    else
+      record.customer_record = customer.record
+    end
   end
 end
--- test/order/entity_test.rb
+++ test/order/entity_test.rb
@@ -2,12 +2,22 @@ require_relative '../test_config'
 
 class Order
   class EntityTest < MyRecordTest
-    def item
-      @item ||= Item.new record
+    def order
+      @order ||= Order.new record
     end
 
     def record
       @record ||= OpenStruct.new
+    end
+
+    def test_it_should_set_state_to_pending_if_none
+      order
+      assert_equal 'pending', record.state
+    end
+
+    def test_it_should_use_state_if_given
+      record.state = 'processing'
+      assert_equal 'processing', order.state
     end
 
     # def test_creation
@@ -23,5 +33,38 @@ class Order
     #
     #   order.setup('http://www.example.com').redirect_uri
     # end
+
+    ################# Associations #####################
+
+    def test_can_make_customer
+      record.customer_record = :customer
+      assert_equal Customer.new(:customer), order.customer
+    end
+
+    def test_nil_customer_if_no_record
+      assert_nil order.customer
+    end
+
+    def test_can_set_customer
+      order.customer = Customer.new(:customer)
+      assert_equal :customer, record.customer_record
+    end
+
+    def test_can_set_nil_customer
+      record.customer_record = :customer
+      order.customer = nil
+    end
+
+    ################# Archive #####################
+
+    def test_can_access_state
+      record.state = 'pending'
+      assert_equal 'pending', order.state
+    end
+
+    def test_can_set_first_name
+      order.state = 'pending'
+      assert_equal 'pending', record.state
+    end
   end
 end
