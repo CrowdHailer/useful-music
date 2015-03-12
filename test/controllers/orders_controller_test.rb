c993bd3c67cd478a5e5f41cc6b67d271519c55c2
Peter Saxton <peterhsaxton@gmail.com>
2015-03-12 10:28:18 +0000

    order spike

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -31,6 +31,17 @@ class OrdersControllerTest < MyRecordTest
     assert_equal order.basket_amount, shopping_basket.price
     assert_equal order.record.state, 'processing'
     assert last_response.redirect?
+    assert_includes last_response.location, 'sandbox'
     # ap last_response.location
+  end
+
+  def test_creates_order_with_discount
+    discount_record = create :discount_record
+    shopping_basket_record = create :shopping_basket_record
+    shopping_basket = ShoppingBasket.new shopping_basket_record
+    shopping_basket_record.add_purchase_record create(:purchase_record)
+    customer.record.shopping_basket_record = shopping_basket_record
+    customer.record.save
+    post '/', {}, {'rack.session' => {:user_id => customer.id}}
   end
 end
