62331aea8dc018bd01b7665cd7da8cabf2e0f738
Peter Saxton <peterhsaxton@gmail.com>
2015-03-12 09:25:01 +0000

    spike checkout form

--- app/controllers/orders.rb
+++ app/controllers/orders.rb
@@ -6,16 +6,16 @@ class OrdersController < UsefulMusic::App
   render_defaults[:dir] += '/orders'
 
   def create
-    form = Order::Create::Form.new request.POST['order']
-    form.customer = current_customer
-    order = Orders.create(form.to_hash) do |order|
-      order.calculate_prices
-      order.transaction
-    end
-    current_customer.shopping_basket = nil
-    Customers.save current_customer
-    session['guest.shopping_basket'] = nil
-    redirect order.setup(url).redirect_uri
+    form = Order::Create::Form.new request.POST['order'], current_customer
+    # form.customer = current_customer
+    # order = Orders.create(form.to_hash) do |order|
+    #   order.calculate_prices
+    #   order.transaction
+    # end
+    # current_customer.shopping_basket = nil
+    # Customers.save current_customer
+    # session['guest.shopping_basket'] = nil
+    # redirect order.setup(url).redirect_uri
   end
 
   def show(id)
