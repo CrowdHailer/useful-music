f54e0332f20ec55d5daa86cd17a45f8cbc85bad5
Peter Saxton <peterhsaxton@gmail.com>
2015-03-16 22:23:37 +0000

    reduce seed file

--- db/seed.rb
+++ db/seed.rb
@@ -7,42 +7,6 @@ Customers.create(
   :admin => true
 )
 
-Customers.create(
-  :first_name => 'Daisy',
-  :last_name => 'Hill',
-  :email => 'daisy@usefulmusic.com',
-  :password => 'password',
-  :country => Country.new('GB'),
-  :admin => true
-)
-
-Customers.create(
-  :first_name => 'Richard',
-  :last_name => 'Shepherd',
-  :email => 'richard@richardesigns.co.uk',
-  :password => 'password',
-  :country => Country.new('GB'),
-  :admin => true
-)
-
-Customers.create(
-  :first_name => 'Graham',
-  :last_name => 'Lyons',
-  :email => 'usefulmusic@gmail.com',
-  :password => 'password',
-  :country => Country.new('GB'),
-  :admin => true
-)
-
-100.times do |i|
-  Customers.create(
-    :first_name => ['dave', 'clare', 'steve', 'amy', 'Jasmin', 'Dan', 'Lewis', 'Jess'].sample,
-    :last_name => ['smith', 'jones', 'davis', 'edwards'].sample,
-    :email => "my_account_#{i}@example.com",
-    :password => 'password',
-    :country => Country.new('GB')
-  )
-end
 
 # Bundler.require :test
 require 'factory_girl'
@@ -81,18 +45,3 @@ FactoryGirl.create :item_record,
   :initial_price => Money.new(100),
   :discounted_price => Money.new(80),
   :asset => Rack::Test::UploadedFile.new('test/fixtures/UD477kbd.pdf', 'application/pdf')
-
-100.times do
-  FactoryGirl.create :piece_record,
-    :beginner => [true, false].sample,
-    :intermediate => [true, false].sample,
-    :advanced => [true, false].sample,
-    :solo => [true, false, false].sample,
-    :duet => [true, false, false].sample,
-    :trio => [true, false, false].sample,
-    :larger_ensembles => [true, false, false].sample,
-    :piano => [true, false, false].sample,
-    :recorder => [true, false, false].sample,
-    :flute => [true, false, false].sample,
-    :oboe => [true, false, false].sample
-end
