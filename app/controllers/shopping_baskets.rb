e2d43f55178d22803dc1b87c8c1962a578b0339c
Peter Saxton <peterhsaxton@gmail.com>
2015-03-19 14:39:57 +0000

    redirect from not found basket update

--- app/controllers/shopping_baskets.rb
+++ app/controllers/shopping_baskets.rb
@@ -3,12 +3,16 @@ class ShoppingBasketsController < UsefulMusic::App
   render_defaults[:dir] += '/shopping_baskets'
 
   def show(id)
-    @basket = ShoppingBaskets.fetch(id) { redirect '/' }
+    @basket = ShoppingBaskets.fetch(id, &method(:shopping_basket_not_found))
     render :show
   end
 
+  def update(id)
+    ShoppingBaskets.fetch(id, &method(:shopping_basket_not_found))
+  end
+
   def destroy(id)
-    basket = ShoppingBaskets.fetch(id) { redirect '/' }
+    basket = ShoppingBaskets.fetch(id, &method(:shopping_basket_not_found))
     customer = current_customer
     unless customer.guest?
       customer.record.update :shopping_basket_record => nil
@@ -16,6 +20,11 @@ class ShoppingBasketsController < UsefulMusic::App
     ShoppingBaskets.remove basket
     flash['success'] = 'Shopping basket cleared'
     redirect '/'
+  end
+
+  def shopping_basket_not_found(id)
+    flash['error'] = 'Shopping Basket not found'
+    redirect request.referer
   end
 
 end
--- test/controllers/shopping_basket_controller_test.rb
+++ test/controllers/shopping_basket_controller_test.rb
@@ -7,7 +7,7 @@ class ShoppingBasketsControllerTest < MyRecordTest
     ShoppingBasketsController
   end
 
-  def test_redirects_when_basket_not_found
+  def test_redirects_when_basket_not_found_to_show
     get '/1'
     assert last_response.redirect?
   end
@@ -20,6 +20,18 @@ class ShoppingBasketsControllerTest < MyRecordTest
   def test_shows_shopping_basket_to_customer
     shopping_basket_record = create :shopping_basket_record
     assert_ok get("/#{shopping_basket_record.id}", {}, {'rack.session' => {:user_id => customer.id}})
+  end
+
+  def test_can_update_basket_with_discount_code
+    discount_record = create :discount_record
+    shopping_basket_record = create :shopping_basket_record
+    patch "/#{shopping_basket_record.id}", {:shopping_basket => {:discount => discount_record.code}}
+    skip
+  end
+
+  def test_redirect_when_editing_non_existant_basket
+    patch "/1", {:shopping_basket => {:discount => 3}}
+    assert last_response.redirect?
   end
 
   def test_can_destroy_guest_shopping_basket
@@ -35,7 +47,7 @@ class ShoppingBasketsControllerTest < MyRecordTest
     delete "/#{shopping_basket_record.id}", {}, {'rack.session' => {:user_id => customer.id}}
   end
 
-  def test_redirects_if_basket_not_found
+  def test_redirects_if_basket_not_found_to_delete
     delete "/1", {}, {'rack.session' => {:user_id => customer.id}}
     assert last_response.redirect?
   end
