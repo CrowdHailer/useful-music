dcf84362c8edc489ec663dd239bd82e9869f4d4a
Peter Saxton <peterhsaxton@gmail.com>
2015-03-19 08:50:07 +0000

    imput bulgeim rate

--- lib/country/eu_vat.rb
+++ lib/country/eu_vat.rb
@@ -1,11 +1,49 @@
 class Country
   class EUVAT
+    DEFAULT_RATE = 0
+    RATES = {
+      Country.new('AT').alpha2 => 0.21,
+      Country.new('BE').alpha2 => 0.21,
+      Country.new('BG').alpha2 => 0.21,
+      Country.new('HR').alpha2 => 0.21,
+      Country.new('CY').alpha2 => 0.21,
+      Country.new('CZ').alpha2 => 0.21,
+      Country.new('DK').alpha2 => 0.21,
+      Country.new('EE').alpha2 => 0.21,
+      Country.new('FI').alpha2 => 0.21,
+      Country.new('FR').alpha2 => 0.21,
+      Country.new('DE').alpha2 => 0.21,
+      Country.new('GR').alpha2 => 0.21,
+      Country.new('HU').alpha2 => 0.21,
+      Country.new('IE').alpha2 => 0.21,
+      Country.new('IT').alpha2 => 0.21,
+      Country.new('LV').alpha2 => 0.21,
+      Country.new('LT').alpha2 => 0.21,
+      Country.new('LU').alpha2 => 0.21,
+      Country.new('MT').alpha2 => 0.21,
+      Country.new('NL').alpha2 => 0.21,
+      Country.new('PL').alpha2 => 0.21,
+      Country.new('PT').alpha2 => 0.21,
+      Country.new('RO').alpha2 => 0.21,
+      Country.new('SK').alpha2 => 0.21,
+      Country.new('SI').alpha2 => 0.21,
+      Country.new('ES').alpha2 => 0.21,
+      Country.new('SE').alpha2 => 0.21,
+      Country.new('GB').alpha2 => 0.21
+    }
     def initialize(country)
-
+      @country = country
+      @rates = Hash.new(DEFAULT_RATE).merge RATES
     end
 
+    attr_reader :country, :rates
+
     def to_f
-      0
+      rates[country.alpha2]
+    end
+
+    def *(n)
+      to_f * n
     end
 
   end
--- test/country/eu_vat_test.rb
+++ test/country/eu_vat_test.rb
@@ -6,9 +6,28 @@ class Country
       Country.new('AF')
     end
 
+    def belgium
+      Country.new('BE')
+    end
+
     def test_afghanistan_uses_the_default_rate
       rate = EUVAT.new afghanistan
       assert_equal 0, rate.to_f
+    end
+
+    def test_can_access_country
+      rate = EUVAT.new afghanistan
+      assert_equal afghanistan, rate.country
+    end
+
+    def test_rate_can_multiply_number
+      rate = EUVAT.new belgium
+      assert_equal 0.21, rate * 1
+    end
+
+    def test_can_multiply_money
+      rate = EUVAT.new belgium
+      assert_equal Money.new(21, 'EUR'), rate * Money.new(100, 'EUR')
     end
 
   end
