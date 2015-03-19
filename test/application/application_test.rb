79f42ab83113f943e593adb7beccfa94e1776d7b
Peter Saxton <peterhsaxton@gmail.com>
2015-03-19 10:17:40 +0000

    500 error view

--- test/application/application_test.rb
+++ test/application/application_test.rb
@@ -18,5 +18,12 @@ module UsefulMusic
       get '/random'
       assert_equal 404, last_response.status
     end
+
+    def test_500_when_error
+      skip
+      @app = UsefulMusic::App
+      get '/test-error'
+      assert_equal 500, last_response.status
+    end
   end
 end
