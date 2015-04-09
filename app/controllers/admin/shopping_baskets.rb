014327e2cbc71705208108607afe5a8e3eec4d45
Peter Saxton <peterhsaxton@gmail.com>
2015-04-09 17:26:45 +0100

    catch error from reference baskets

--- app/controllers/admin/shopping_baskets.rb
+++ app/controllers/admin/shopping_baskets.rb
@@ -10,6 +10,18 @@ module UsefulMusic
         @shopping_baskets = ShoppingBaskets.new request.GET
         render :index
       end
+
+      def destroy(id)
+        discount = ShoppingBaskets[id]
+        begin
+          ShoppingBaskets.remove(discount)
+          flash['success'] = 'Discount Deleted'
+          redirect '/admin/shopping_baskets'
+        rescue Sequel::ForeignKeyConstraintViolation => err
+          flash['error'] = 'Shopping Basket could not be cleared it is still referenced'
+          redirect '/admin/shopping_baskets'
+        end
+      end
     end
   end
 end
