45466bf30193b7e5feaf329cc63d81bfea3abcf5
CrowdHailer <peterhsaxton@gmail.com>
2016-11-23 17:59:10 +0000

    test the creation endpoint for new pieces

--- project/apps/web/lib/web/admin/pieces.ex
+++ project/apps/web/lib/web/admin/pieces.ex
@@ -1,14 +1,82 @@
 defmodule UM.Web.Admin.Pieces do
   require EEx
 
+  @piece_types %{
+    id: :integer,
+    title: :string,
+    sub_heading: :string,
+    level_overview: :string,
+    description: :string,
+    solo: :boolean,
+    solo_with_accompaniment: :boolean,
+    duet: :boolean,
+    trio: :boolean,
+    quartet: :boolean,
+    larger_ensembles: :boolean,
+    collection: :boolean,
+    beginner: :boolean,
+    intermediate: :boolean,
+    advanced: :boolean,
+    professional: :boolean,
+    piano: :boolean,
+    recorder: :boolean,
+    flute: :boolean,
+    oboe: :boolean,
+    clarineo: :boolean,
+    clarinet: :boolean,
+    bassoon: :boolean,
+    saxophone: :boolean,
+    trumpet: :boolean,
+    violin: :boolean,
+    viola: :boolean,
+    percussion: :boolean,
+    notation_preview: "", # Hash
+    audio_preview: "", # Hash
+    cover_image: "", # Hash
+    print_link: "", # String
+    print_title: "", # String
+    weezic_link: "", # String
+    meta_description: "", # String
+    meta_keywords: "", # String
+  }
+
   new_file = String.replace_suffix(__ENV__.file, ".ex", "/new.html.eex")
   EEx.function_from_file :def, :new_page_content, new_file, []
 
-  def handle_request(%{path: []}, _env) do
+  def handle_request(%{path: [], method: :GET}, _env) do
     Raxx.Response.ok("All pieces TODO show UM100")
   end
 
-  def handle_request(%{path: ["search"], query: query}, _env) do
+  def handle_request(request = %{path: [], method: :POST}, _env) do
+    form = Plug.Conn.Query.decode(request.body) |> Map.get("piece")
+    piece = Enum.map(@piece_types, fn
+      ({key, :boolean}) ->
+        case Map.get(form, "#{key}", "") do
+          "on" -> {key, true}
+          "" -> {key, false}
+        end
+      ({key, :integer}) ->
+        case Map.get(form, "#{key}", "") |> Integer.parse do
+          {i, ""} -> {key, i}
+          _ -> {key, nil}
+        end
+      ({key, v}) ->
+        {key, v}
+    end)
+    |> Enum.into(%{})
+    case UM.Catalogue.create_piece(piece) do
+      {:ok, piece} ->
+        Raxx.Response.found("", [{"location", "/admin/pieces"}])
+      {:error, :invalid_piece} ->
+        # loose all data but thats just ok on the admin side, at the moment
+        Raxx.Response.found("", [{"location", "/admin/pieces/new"}])
+      {:error, :id_already_used} ->
+        # TODO change to sending to the edit page
+        Raxx.Response.found("", [{"location", "/admin/pieces/new"}])
+    end
+  end
+
+  def handle_request(%{path: ["search"], method: :GET, query: query}, _env) do
     search = Map.get(query, "search")
     id = case search do
       "UD" <> id ->
--- project/apps/web/test/web/admin/pieces_test.exs
+++ project/apps/web/test/web/admin/pieces_test.exs
@@ -34,5 +34,66 @@ defmodule UM.Web.Admin.PiecesTest do
     %{status: status, body: body} = Pieces.handle_request(request, %{})
     assert 200 == status
     assert String.contains?(body, "New Piece")
+
+  end
+
+  test "can create a new piece" do
+    request = %Raxx.Request{
+      path: [],
+      method: :POST,
+      body: Plug.Conn.Query.encode(%{
+        piece: %{
+          id: "123",
+          title: "A piece of music",
+          sub_heading: "flute and claranet",
+          level_overview: "1 to 3",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+        }),
+        headers: [{"content-type", "application/x-www-form-urlencoded"}]
+      }
+      %{status: status, headers: headers} = Pieces.handle_request(request, %{})
+      assert 302 == status
+      assert {"location", "/admin/pieces"} == List.keyfind(headers, "location", 0)
+  end
+
+  test "cant create a piece without id" do
+    request = %Raxx.Request{
+      path: [],
+      method: :POST,
+      body: Plug.Conn.Query.encode(%{
+        piece: %{
+          id: "",
+          title: "A piece of music",
+          sub_heading: "flute and claranet",
+          level_overview: "1 to 3",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+        }),
+        headers: [{"content-type", "application/x-www-form-urlencoded"}]
+      }
+      %{status: status, headers: headers} = Pieces.handle_request(request, %{})
+      assert 302 == status
+      assert {"location", "/admin/pieces/new"} == List.keyfind(headers, "location", 0)
+  end
+
+  test "cant create a piece with existing id" do
+    request = %Raxx.Request{
+      path: [],
+      method: :POST,
+      body: Plug.Conn.Query.encode(%{
+        piece: %{
+          id: "100",
+          title: "A piece of music",
+          sub_heading: "flute and claranet",
+          level_overview: "1 to 3",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+        }),
+        headers: [{"content-type", "application/x-www-form-urlencoded"}]
+      }
+      %{status: status, headers: headers} = Pieces.handle_request(request, %{})
+      assert 302 == status
+      assert {"location", "/admin/pieces/new"} == List.keyfind(headers, "location", 0)
   end
 end
