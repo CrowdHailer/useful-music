5eab9ec1be37a130d7ae8d4b7eecc6902744f676
CrowdHailer <peterhsaxton@gmail.com>
2016-11-28 11:34:30 +0000

    all delivery tests for the admin customer panel

--- project/apps/web/lib/web/admin/customers.ex
+++ project/apps/web/lib/web/admin/customers.ex
@@ -5,10 +5,12 @@ defmodule UM.Web.Admin.Customers do
   EEx.function_from_file :def, :index_page_content, index_file, [:page]
 
   def handle_request(%{method: :GET, path: [], query: query}, _) do
+    # TODO decide if query can be nil
+    query = query || %{}
     size = Map.get(query, "page_size", "10") |> :erlang.binary_to_integer
     page = Map.get(query, "page", "1") |> :erlang.binary_to_integer
     customers = [
-      %{id: 23, name: "bill", email: "bill@yahoo.com", admin: false},
+      %{id: 23, name: "Issac", email: "issac@yahoo.com", admin: false},
       %{id: 23, name: "susan", email: "susan@yahoo.com", admin: true},
     ]
     Raxx.Response.ok(UM.Web.Admin.layout_page(index_page_content(%{
@@ -20,7 +22,18 @@ defmodule UM.Web.Admin.Customers do
       })))
   end
 
+  def handle_request(request = %{method: :POST, path: [id, "admin"]}, _) do
+    # IO.inspect(id)
+    {:ok, form} = Raxx.Request.content(request)
+    case Map.get(form, "_method") do
+      nil ->
+        Raxx.Response.see_other("", [{"location", "/admin/customers"}])
+      "DELETE" ->
+        Raxx.Response.see_other("", [{"location", "/admin/customers"}])
+    end
+  end
+
   def csrf_tag do
-    "TODO"
+    "todo csrf tag"
   end
 end
--- project/apps/web/test/web/admin/customers_test.exs
+++ project/apps/web/test/web/admin/customers_test.exs
@@ -7,6 +7,24 @@ defmodule UM.Web.Admin.CustomersTest do
   test "index page shows customer" do
     request = get("/")
     response = Customers.handle_request(request, :nostate)
-    assert String.contains?(response.body, "issac")
+    assert String.contains?(response.body, "Issac")
+  end
+
+  test "can make customer an admin" do
+    request = post("/dummy-customer-id/admin", form_data(%{}))
+    response = Customers.handle_request(request, :nostate)
+    # TODO test repository edited
+    # TODO test flash added to location
+    assert "/admin/customers" == Raxx.Patch.response_location(response)
+  end
+
+  # Same test for delete and missing
+
+  test "can make remove admin access" do
+    request = post("/dummy-customer-id/admin", form_data(_method: "DELETE"))
+    response = Customers.handle_request(request, :nostate)
+    # TODO test repository
+    # TODO test flash
+    assert "/admin/customers" == Raxx.Patch.response_location(response)
   end
 end
