628be7fa61ff5d3738716206a13c3915d7d3b4e3
CrowdHailer <peterhsaxton@gmail.com>
2016-11-28 11:17:04 +0000

    print customer information on page

--- project/apps/web/lib/web/admin/customers.ex
+++ project/apps/web/lib/web/admin/customers.ex
@@ -4,13 +4,19 @@ defmodule UM.Web.Admin.Customers do
   index_file = String.replace_suffix(__ENV__.file, ".ex", "/index.html.eex")
   EEx.function_from_file :def, :index_page_content, index_file, [:page]
 
-  def handle_request(_, _) do
+  def handle_request(%{method: :GET, path: [], query: query}, _) do
+    size = Map.get(query, "page_size", "10") |> :erlang.binary_to_integer
+    page = Map.get(query, "page", "1") |> :erlang.binary_to_integer
+    customers = [
+      %{id: 23, name: "bill", email: "bill@yahoo.com", admin: false},
+      %{id: 23, name: "susan", email: "susan@yahoo.com", admin: true},
+    ]
     Raxx.Response.ok(UM.Web.Admin.layout_page(index_page_content(%{
-      size: 10,
-      previous: false,
-      next: 2,
-      last: 1,
-      customers: []
+      size: size,
+      previous: max(page - 1, 0),
+      next: page + 1,
+      last: 10,
+      customers: Enum.zip(customers, 1..Enum.count(customers))
       })))
   end
 
