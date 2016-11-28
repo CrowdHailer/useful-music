bb18a8a634d6464f4ab1dfeb49e41dfcb179e0c7
CrowdHailer <peterhsaxton@gmail.com>
2016-11-28 17:50:04 +0000

    add email validation to form

--- project/apps/web/lib/web/customers/create_form.ex
+++ project/apps/web/lib/web/customers/create_form.ex
@@ -13,38 +13,55 @@ defmodule UM.Web.Customers.CreateForm do
   ]
 
   def validate(form) do
-    data = %__MODULE__{}
+    data = %__MODULE__{} # should be action struct signup not signupform
     errors = %__MODULE__{}
 
-    {data, errors} = case validate_name(Map.get(form, "first_name")) do
+    {data, errors} = case validate_name(Map.get(form, "first_name", "")) do
       {:ok, first_name} ->
         {Map.merge(data, %{first_name: first_name}), Map.merge(errors, %{first_name: nil})}
       {:error, reason} ->
         {Map.merge(data, %{first_name: nil}), Map.merge(errors, %{first_name: reason})}
     end
 
-    {data, errors} = case validate_name(Map.get(form, "last_name")) do
+    {data, errors} = case validate_name(Map.get(form, "last_name", "")) do
       {:ok, last_name} ->
         {Map.merge(data, %{last_name: last_name}), Map.merge(errors, %{last_name: nil})}
       {:error, reason} ->
         {Map.merge(data, %{last_name: nil}), Map.merge(errors, %{last_name: reason})}
     end
+
+    {data, errors} = case validate_email(Map.get(form, "email", "")) do
+      {:ok, email} ->
+        {Map.merge(data, %{email: email}), Map.merge(errors, %{email: nil})}
+      {:error, reason} ->
+        {Map.merge(data, %{email: nil}), Map.merge(errors, %{email: reason})}
+    end
   end
 
-  def validate_name(nil) do
+  # TODO validate contains letters only
+  def validate_name("") do
     # This is an odd error because it should not be part of the validator
     {:error, "name is required"}
   end
   def validate_name(text) do
     name = String.strip(text) |> String.capitalize
     case String.length(name) >= 2 do
-      true ->
-        {:ok, name}
       false ->
         {:error, "name is too short"}
+      true ->
+        case String.length(name) <= 26 do
+          false ->
+            {:error, "name is too long"}
+          true ->
+            {:ok, name}
+        end
     end
   end
 
+  defp validate_email("") do
+    # This is an odd error because it should not be part of the validator
+    {:error, "email is required"}
+  end
   defp validate_email(raw) do
     case String.split(raw, "@") do
       [_, _] ->
--- project/apps/web/test/web/customers/create_form_test.exs
+++ project/apps/web/test/web/customers/create_form_test.exs
@@ -3,7 +3,7 @@ defmodule UM.Web.Customers.CreateFormTest do
 
   alias UM.Web.Customers.CreateForm
 
-  test "cleans valid first name" do
+  test "coerces first name" do
     {data, errors} = CreateForm.validate(%{"first_name" => "  will"})
     assert data.first_name == "Will"
     assert errors.first_name == nil
@@ -15,9 +15,39 @@ defmodule UM.Web.Customers.CreateFormTest do
     assert errors.first_name == "name is too short"
   end
 
+  test "long name is invalid" do
+    {data, errors} = CreateForm.validate(%{"first_name" => "1111111111111111111111111111"})
+    assert data.first_name == nil
+    assert errors.first_name == "name is too long"
+  end
+
+  test "requires a first name" do
+    {data, errors} = CreateForm.validate(%{"first_name" => ""})
+    assert data.first_name == nil
+    assert errors.first_name == "name is required"
+  end
+
   test "cleans valid last name" do
     {data, errors} = CreateForm.validate(%{"last_name" => " Smith  "})
     assert data.last_name == "Smith"
     assert errors.last_name == nil
+  end
+
+  test "requires a last name" do
+    {data, errors} = CreateForm.validate(%{"last_name" => ""})
+    assert data.last_name == nil
+    assert errors.last_name == "name is required"
+  end
+
+  test "cleans email" do
+    {data, errors} = CreateForm.validate(%{"email" => " Test@ExamplE.com  "})
+    assert data.email == "test@example.com"
+    assert errors.email == nil
+  end
+
+  test "requires an email" do
+    {data, errors} = CreateForm.validate(%{"email" => ""})
+    assert data.email == nil
+    assert errors.email == "email is required"
   end
 end
