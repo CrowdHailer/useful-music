469dbfd46f20195967a8bac3d331bfc0a62285e2
CrowdHailer <peterhsaxton@gmail.com>
2016-12-12 15:50:58 +0000

    delete old purchase domain items

--- lib/purchases/inquiry.rb
+++
@@ -1,9 +0,0 @@
-class Purchases
-  class Inquiry < Errol::Inquiry
-    default :order, :id
-    default :page, 1
-    default :page_size, 15
-    default :shopping_basket, nil
-    default :item, nil
-  end
-end
