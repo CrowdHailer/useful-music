147f696d68c9399b349664e9766a55ac57e2c5e1
CrowdHailer <peterhsaxton@gmail.com>
2016-12-12 15:53:59 +0000

    remove some basket code

--- lib/shopping_baskets/inquiry.rb
+++
@@ -1,9 +0,0 @@
-class ShoppingBaskets
-  class Inquiry < Errol::Inquiry
-    default :order, :id
-    default :page, 1
-    default :page_size, 15
-    default :checked_out, false
-    default :discount, nil
-  end
-end
