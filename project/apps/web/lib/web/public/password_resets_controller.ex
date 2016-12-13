e184f84abc103bd495b522d8956272f2a9be746c
CrowdHailer <peterhsaxton@gmail.com>
2016-12-13 11:21:26 +0000

    setup create password reset page

--- project/apps/web/lib/web/public/password_resets_controller.ex
+++ project/apps/web/lib/web/public/password_resets_controller.ex
@@ -1,15 +1,26 @@
 defmodule UM.Web.PasswordResetsController do
-  def handle_request(%{path: [], method: :GET}) do
-    Raxx.Response.ok("hello")
+  require EEx
+
+  new_file = String.replace_suffix(__ENV__.file, ".ex", "/new.html.eex")
+  EEx.function_from_file :def, :new_page_content, new_file, [:unknown_email]
+
+
+  def handle_request(%{path: ["new"], method: :GET}, _) do
+    Raxx.Response.ok(new_page_content(nil))
   end
 
-  def handle_request(%{path: [], method: :POST, body: %{"customer" => form}}) do
-    case UM.Accounts.create_password_reset(Map.get(form, "email", "")) do
+  def handle_request(%{path: [], method: :POST, body: %{"customer" => form}}, _) do
+    email = Map.get(form, "email", "")
+    case UM.Accounts.create_password_reset(email) do
       {:ok, customer} ->
         # send email
-        Raxx.Patch.redirect("/session/new", success: "A password reset has been sent to your email")
+        Raxx.Patch.redirect("/sessions/new", success: "A password reset has been sent to your email")
       {:error, :no_customer} ->
-        Raxx.Response.ok("Email not found")
+        Raxx.Response.ok(new_page_content(email))
     end
+  end
+
+  def csrf_tag do
+    "" # TODO
   end
 end
--- project/apps/web/test/web/public/password_resets_controller_test.exs
+++ project/apps/web/test/web/public/password_resets_controller_test.exs
@@ -4,20 +4,24 @@ defmodule UM.Web.PasswordResetsControllerTest do
 
   alias UM.Web.PasswordResetsController, as: Controller
 
+  setup do
+    :ok = UM.Web.Fixtures.clear_db
+  end
+
   test "new page is available" do
-    request = get("/")
-    response = Controller.handle_request(request)
+    request = get("/new")
+    response = Controller.handle_request(request, :nostate)
     assert 200 = response.status
   end
 
   test "create password reset" do
     jo = UM.Web.Fixtures.jo_brand
     request = post("/", form_data(%{"customer" => %{"email" => jo.email}}))
-    |> Controller.handle_request
+    |> Controller.handle_request(:nostate)
     |> Raxx.Patch.follow
     {flash, request} = UM.Web.Flash.from_request(request)
     assert %{success: "A password reset has been sent to your email"} = flash
-    assert ["session", "new"] == request.path
+    assert ["sessions", "new"] == request.path
   end
 
   test "rerenders when email not found" do
