e9ffe2bc5aaa0682d158020f5f4553ccebe44643
CrowdHailer <peterhsaxton@gmail.com>
2016-12-27 13:09:37 +0000

    spike orders controller

---
+++ apps/web/lib/web/public/orders_controller.ex
@@ -0,0 +1,16 @@
+defmodule UM.Web.OrdersController do
+  # checkout
+  # def handle_request() do
+  #   session = UM.Web.fetch_session(request)
+  #   cart = UM.Web.Session.cart(session)
+  #   ^cart_id = cart.id || "__empty__"
+  #
+  #   {:ok, order} = Cart.place_order
+  #   Order.process(order)
+  #   {:ok, %{status: complete}}
+  #   # redirection
+  #   # error
+  #   Order.success
+  #   Order.cancel
+  # end
+end
