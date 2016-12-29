9f4c9feb6170b6076c44ecca376af0bb7cfd5c4b
CrowdHailer <peterhsaxton@gmail.com>
2016-12-29 11:06:01 +0000

    add stuff to patch

--- apps/utils/lib/raxx_patch.ex
+++ apps/utils/lib/raxx_patch.ex
@@ -1,4 +1,25 @@
 defmodule Raxx.Patch do
+  @doc """
+  Adds a set cookie header to the response.
+
+  For options see `Raxx.Cookie.Attributes`
+  """
+  def set_cookie(r = %{headers: headers}, key, value, options \\ %{}) do
+    cookies = Map.get(headers, "set-cookie", [])
+    %{r | headers: Map.merge(headers, %{"set-cookie" => cookies ++ [Raxx.Cookie.new(key, value, options) |> Raxx.Cookie.set_cookie_string]})}
+  end
+
+  @doc """
+  Adds a cookie header to the response, that will expire the cookie with the given key.
+
+  **NOTE:** Will not expire session cookies.
+  """
+  def expire_cookie(r = %{headers: headers}, key) do
+    cookies = Map.get(headers, "set-cookie", [])
+    %{r | headers: %{"set-cookie" => cookies ++ ["#{key}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/"]}}
+  end
+
+
   def get_header(request, key) do
     # check {:ok value} semantics
     {^key, value} = List.keyfind(request.headers, key, 0)
@@ -74,6 +95,29 @@ defmodule Raxx.Patch do
     """
       <html><body>You are being <a href=\"#{ escape(path) }\">redirected</a>.</body></html>
     """
+  end
+  # For redirect consider string/tuple/map url
+  # consider mount middleware adjusting paths
+  # consider marking redirects as absolute
+
+  # needed for redirect
+  @escapes [
+    {?<, "&lt;"},
+    {?>, "&gt;"},
+    {?&, "&amp;"},
+    {?", "&quot;"},
+    {?', "&#39;"}
+  ]
+
+  Enum.each @escapes, fn { match, insert } ->
+    defp escape_char(unquote(match)), do: unquote(insert)
+  end
+
+  defp escape_char(char), do: << char >>
+
+  @doc false
+  defp escape(buffer) do
+    IO.iodata_to_binary(for <<char <- buffer>>, do: escape_char(char))
   end
 
   def follow(response) do
