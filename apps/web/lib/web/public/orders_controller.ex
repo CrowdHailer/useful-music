4ea32a47fd529c529ade184b603f93731eed7185
CrowdHailer <peterhsaxton@gmail.com>
2017-01-04 20:03:43 +0000

    use existing functions for items in an order

--- apps/web/lib/web/public/orders_controller.ex
+++ apps/web/lib/web/public/orders_controller.ex
@@ -17,13 +17,14 @@ defmodule UM.Web.OrdersController do
   require EEx
 
   show_template = String.replace_suffix(__ENV__.file, ".ex", "/show.html.eex")
-  EEx.function_from_file :def, :show_page, show_template, [:order, :cart]
+  EEx.function_from_file :def, :show_page, show_template, [:order, :cart, :session]
 
-  def handle_request(%{path: [id], method: :GET}, _) do
+  def handle_request(request = %{path: [id], method: :GET}, _) do
+    session = UM.Web.fetch_session(request)
     case UM.Sales.OrdersRepo.fetch_by_id(id) do
       {:ok, order} ->
         {:ok, cart} = UM.Sales.CartsRepo.fetch_by_id(order.shopping_basket_id)
-        {:no_layout, Raxx.Response.ok(show_page(order, cart))}
+        {:no_layout, Raxx.Response.ok(show_page(order, cart, session))}
     end
   end
 
