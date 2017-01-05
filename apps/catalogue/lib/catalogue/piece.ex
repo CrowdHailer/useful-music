7c742bcd7435dfff7d872256253862d9142307b2
CrowdHailer <peterhsaxton@gmail.com>
2017-01-05 07:38:36 +0000

    remove duplicate tag lists

--- apps/catalogue/lib/catalogue/piece.ex
+++ apps/catalogue/lib/catalogue/piece.ex
@@ -1,94 +1,5 @@
 defmodule UM.Catalogue.Piece do
-  defstruct [
-    id: nil, # Integer
-    title: nil, # String
-    sub_heading: nil, # String
-    level_overview: nil, # String
-    description: nil, # String
-    solo: false,
-    solo_with_accompaniment: false,
-    duet: false,
-    trio: false,
-    quartet: false,
-    larger_ensembles: false,
-    collection: false,
-    beginner: false,
-    intermediate: false,
-    advanced: false,
-    professional: false,
-    piano: false,
-    recorder: false,
-    flute: false,
-    oboe: false,
-    clarineo: false,
-    clarinet: false,
-    bassoon: false,
-    saxophone: false,
-    trumpet: false,
-    violin: false,
-    viola: false,
-    percussion: false,
-    notation_preview: nil, # Hash
-    audio_preview: nil, # Hash
-    cover_image: nil, # Hash
-    print_link: nil, # String
-    print_title: nil, # String
-    weezic_link: nil, # String
-    meta_description: nil, # String
-    meta_keywords: nil, # String
-    items: :not_loaded
-  ]
-
-  def catalogue_number(%{id: id}) do
-    "UD#{id}"
-  end
-
-  def product_name(%{title: title, sub_heading: sub_heading}) do
-    "#{title} - #{sub_heading}"
-  end
-
-  def categories(piece) do
-    Enum.filter([:solo,
-    :solo_with_accompaniment,
-    :duet,
-    :trio,
-    :quartet,
-    :larger_ensembles], fn (key) ->
-      {:ok, bool} = Map.fetch(piece, key)
-      bool
-    end)
-  end
-
-  def instruments(piece) do
-    Enum.filter([:piano,
-    :recorder,
-    :flute,
-    :oboe,
-    :clarineo,
-    :clarinet,
-    :bassoon,
-    :saxophone,
-    :trumpet,
-    :violin,
-    :viola,
-    :percussion], fn (key) ->
-      {:ok, bool} = Map.fetch(piece, key)
-      bool
-    end)
-  end
-
-  def levels(piece) do
-    Enum.filter([:beginner,
-    :intermediate,
-    :advanced,
-    :professional], fn (key) ->
-      {:ok, bool} = Map.fetch(piece, key)
-      bool
-    end)
-  end
-
-  def all_instruments do
-    [:piano,
+  @all_instruments [:piano,
     :recorder,
     :flute,
     :oboe,
@@ -99,22 +10,82 @@ defmodule UM.Catalogue.Piece do
     :trumpet,
     :violin,
     :viola]
-  end
 
-  def all_levels do
-    [:beginner,
+  @all_levels [
+    :beginner,
     :intermediate,
     :advanced,
     :professional]
-  end
 
-  def all_categories do
-    [:solo,
+  @all_categories [:solo,
     :solo_with_accompaniment,
     :duet,
     :trio,
     :quartet,
     :larger_ensembles]
+
+  defstruct ([
+    id: nil, # Integer
+    title: nil, # String
+    sub_heading: nil, # String
+    level_overview: nil, # String
+    description: nil, # String
+    ] ++ for c <- (@all_instruments ++ @all_levels ++ @all_categories) do
+      {c, false}
+    end ++ [
+    collection: false,
+    percussion: false,
+    notation_preview: nil, # Hash
+    audio_preview: nil, # Hash
+    cover_image: nil, # Hash
+    print_link: nil, # String
+    print_title: nil, # String
+    weezic_link: nil, # String
+    meta_description: nil, # String
+    meta_keywords: nil, # String
+    items: :not_loaded
+  ])
+
+  def catalogue_number(%{id: id}) do
+    "UD#{id}"
+  end
+
+  def product_name(%{title: title, sub_heading: sub_heading}) do
+    "#{title} - #{sub_heading}"
+  end
+
+  # score types
+  def categories(piece) do
+    Enum.filter(all_categories, fn (key) ->
+      {:ok, bool} = Map.fetch(piece, key)
+      bool
+    end)
+  end
+
+  def instruments(piece) do
+    Enum.filter(all_instruments, fn (key) ->
+      {:ok, bool} = Map.fetch(piece, key)
+      bool
+    end)
+  end
+
+  def levels(piece) do
+    Enum.filter(all_levels, fn (key) ->
+      {:ok, bool} = Map.fetch(piece, key)
+      bool
+    end)
+  end
+
+  def all_instruments do
+    @all_instruments
+  end
+
+  def all_levels do
+    @all_levels
+  end
+
+  def all_categories do
+    @all_categories
   end
 
 # DEBT these belong in a View layer
